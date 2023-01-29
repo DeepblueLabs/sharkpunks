764bca7b837c1ae6030f61e429d4857202e8f11b
CamiloRX777 <juancamilogallego70@gmail.com>
2023-01-28 17:30:15 -0700

    chore: add openzeppelin and .env

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,19 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import dotenv from 'dotenv';
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
+  networks: {
+    hardhat: {
+    },
+    goerli: {
+      url: `https://goerli.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      accounts: [
+        process.env.DEPLOYER_SIGNATURE_PRIVATE_KEY || ''
+      ]
+    }
+  }
 };
 
 export default config;
